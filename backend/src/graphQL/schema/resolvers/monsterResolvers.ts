089db480fce1295c23ae7c82a552e65067f536f6
andrewmarshall01 <andrew.marshall@frasers.group>
2026-05-14 15:01:59 +0100

    seperate logic

--- backend/src/graphQL/schema/resolvers/monsterResolvers.ts
+++ backend/src/graphQL/schema/resolvers/monsterResolvers.ts
@@ -1,11 +1,8 @@
+import { getMonsterByNameLib } from '../../../lib/monsterHelpers'
 import { Resolvers } from '../../../lib/types/generated'
 
 export const monsterResolvers: Partial<Resolvers> = {
   Query: {
-    getMonsterByName: (_: unknown, args) => ({
-      name: args.name,
-      description: 'yummy',
-      globalRank: 1,
-    }),
+    getMonsterByName: (_: unknown, args) => getMonsterByNameLib(args.name),
   },
 }
---
+++ backend/src/lib/monsterHelpers.ts
@@ -0,0 +1,7 @@
+export const getMonsterByNameLib = (targetName: string) => {
+  return {
+    name: targetName,
+    description: 'yummy',
+    globalRank: 1,
+  }
+}
